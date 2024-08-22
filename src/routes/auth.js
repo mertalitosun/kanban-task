7109d061000be5e7ace5682e029c68b0449199a7
Mertali Tosun <mertali2635@icloud.com>
2024-08-22 22:29:14 +0300

    added logout

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const authController = require("../controllers/auth");
 
-
+router.get("/api/logout",authController.get_logout)
 router.post("/api/register",authController.post_register)
 router.post("/api/login",authController.post_login)
 
