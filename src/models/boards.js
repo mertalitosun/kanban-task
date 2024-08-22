f7ebfb4b1f174c9e4c0f3ecebd0e34e32385d19d
Mertali Tosun <mertali2635@icloud.com>
2024-08-22 21:18:13 +0300

    boards fix

--- src/models/boards.js
+++ src/models/boards.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const boardSchema = mongoose.Schema({
     name:{type:String,required:true},
-    publicId: { type: String, required: true },
+    isPublic: { type: Boolean, default: false },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
 },{timestamps: true})
 module.exports = mongoose.model("Boards", boardSchema)
