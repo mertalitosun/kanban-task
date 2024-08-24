b7e80c76c4024cfce5b9b7372ff686e832e749c9
Mertali Tosun <mertali2635@icloud.com>
2024-08-24 14:38:50 +0300

    comment fix

--- src/routes/user.js
+++ src/routes/user.js
@@ -9,7 +9,7 @@ const {authMiddleware} = require("../middlewares/auth");
 router.post("/api/boards/:id/lists",authMiddleware,userController.post_lists)
 router.get("/api/boards/:id/lists",authMiddleware,userController.get_lists)
 
-router.post("/api/boards/:id",authMiddleware,userController.post_add_member)    //board'a kullanıcı ekleme
+router.post("/api/boards/:id",authMiddleware,userController.post_add_member)//board'a kullanıcı ekleme
 router.get("/api/boards/:id",authMiddleware,userController.get_boards_details)
 router.post("/api/boards",authMiddleware,userController.post_boards)
 router.get("/api/boards",authMiddleware,userController.get_boards)
