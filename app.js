48f12e76309354d9c3a30e89797b4de90b5b111b
Mertali Tosun <mertali2635@icloud.com>
2024-08-27 14:16:16 +0300

    added cors

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 require("express-async-errors")
 const express = require ("express");
 const app = express();
+
 const cookieParser = require('cookie-parser');
+const cors = require("cors");
 
 require("dotenv").config();
 app.use(express.json());
 app.use(cookieParser());
-
 const {errorHandlerMiddleware} = require("./src/middlewares/errorHandler")
 
 //database
@@ -20,11 +21,15 @@ const Cards = require("./src/models/cards")
 const authRoutes = require("./src/routes/auth");
 const userRoutes = require("./src/routes/user");
 
-
 app.use(authRoutes);
 app.use(userRoutes);
 
 
+
+app.use(cors({
+  origin: '*',
+  credentials: true
+}));
 
 //Hata yakalama
 app.use(errorHandlerMiddleware)
