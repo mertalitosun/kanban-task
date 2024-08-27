bcfe250e2badb317033aa367d1b4c97a182a65ca
Mertali Tosun <mertali2635@icloud.com>
2024-08-27 14:47:19 +0300

    fix

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const authController = require("../controllers/auth");
 
-router.get("/api/logout",authController.get_logout)
-router.post("/api/register",authController.post_register)
-router.post("/api/login",authController.post_login)
+router.get("/api/v1/logout", authController.get_logout);
+router.post("/api/v1/register", authController.post_register);
+router.post("/api/v1/login", authController.post_login);
 
 module.exports = router;
--- src/routes/user.js
+++ src/routes/user.js
@@ -5,23 +5,23 @@ const userController = require("../controllers/user");
 
 const {authMiddleware} = require("../middlewares/auth");
 
-router.patch("/api/boards/:boardId/lists/:listId/cards/:cardId",authMiddleware,userController.update_cards);
-router.delete("/api/boards/:boardId/lists/:listId/cards/:cardId",authMiddleware,userController.delete_cards);
-router.post("/api/boards/:boardId/lists/:listId/cards",authMiddleware,userController.post_cards);
+router.patch("/api/v1/boards/:boardId/lists/:listId/cards/:cardId",authMiddleware,userController.update_cards);
+router.delete("/api/v1/boards/:boardId/lists/:listId/cards/:cardId",authMiddleware,userController.delete_cards);
+router.post("/api/v1/boards/:boardId/lists/:listId/cards",authMiddleware,userController.post_cards);
 
-router.patch("/api/boards/:boardId/lists/:listId",authMiddleware,userController.update_lists);
-router.delete("/api/boards/:boardId/lists/:listId",authMiddleware,userController.delete_lists);
-router.post("/api/boards/:boardId/lists",authMiddleware,userController.post_lists);
-router.get("/api/boards/:boardId/lists",authMiddleware,userController.get_lists);
+router.patch("/api/v1/boards/:boardId/lists/:listId",authMiddleware,userController.update_lists);
+router.delete("/api/v1/boards/:boardId/lists/:listId",authMiddleware,userController.delete_lists);
+router.post("/api/v1/boards/:boardId/lists",authMiddleware,userController.post_lists);
+router.get("/api/v1/boards/:boardId/lists",authMiddleware,userController.get_lists);
 
-router.patch("/api/boards/:boardId",authMiddleware,userController.update_boards);
-router.delete("/api/boards/:boardId",authMiddleware,userController.delete_boards);
+router.patch("/api/v1/boards/:boardId",authMiddleware,userController.update_boards);
+router.delete("/api/v1/boards/:boardId",authMiddleware,userController.delete_boards);
 
-router.delete("/api/boards/:boardId/members/:memberId",authMiddleware,userController.delete_members);
-router.post("/api/boards/:boardId/members",authMiddleware,userController.post_add_member);//board'a kullanıcı ekleme
-router.get("/api/boards/:boardId",authMiddleware,userController.get_boards_details);
+router.delete("/api/v1/boards/:boardId/members/:memberId",authMiddleware,userController.delete_members);
+router.post("/api/v1/boards/:boardId/members",authMiddleware,userController.post_add_member);//board'a kullanıcı ekleme
+router.get("/api/v1/boards/:boardId",authMiddleware,userController.get_boards_details);
 
-router.post("/api/boards",authMiddleware,userController.post_boards);
-router.get("/api/boards",authMiddleware,userController.get_boards);
+router.post("/api/v1/boards",authMiddleware,userController.post_boards);
+router.get("/api/v1/boards",authMiddleware,userController.get_boards);
 
 module.exports = router;
