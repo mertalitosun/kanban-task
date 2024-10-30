307831efb73598e5f680bf3b69301723ec7a7017
Mertali Tosun <mertali2635@icloud.com>
2024-10-30 18:00:38 +0300

    fix

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -14,7 +14,7 @@ const errorHandlerMiddleware = (err,req,res,next) =>{
     }
     return res.status(500).json({
         success:false,
-        message:"Sunucu Hatası"
+        message:"Sunucu Hatası",
     })
 }
 
